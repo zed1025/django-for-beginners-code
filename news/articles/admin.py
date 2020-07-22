552385f32ce4812a18831fdec454ef457c661f3b
Amit Kumar <amitkumar@Amits-MacBook-Pro.local>
2020-07-22 21:39:15 +0530

    chapter14-15: permission, authorization, comments on posts

--- news/articles/admin.py
+++ news/articles/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 
-from articles.models import Article
+from articles.models import Article, Comment
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
 
 # Register your models here.
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
